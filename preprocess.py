987129c85053d5d5a609987fbb4170dde38d30fa
Alex Barcelo <alex@betarho.net>
2019-07-26 11:28:26 +0200

    Changed a little bit the strategy.
    NODE_NAME should be provided, which is available through standard
    Downward API:
    https://kubernetes.io/docs/tasks/inject-data-application/environment-variable-expose-pod-information/#the-downward-api
    
    From there, obtain the context.

--- preprocess.py
+++ preprocess.py
@@ -3,14 +3,14 @@
 from kubernetes import client, config
 from jinja2 import Template
 import os
+import sys
 
 config.load_incluster_config()
 
 v1 = client.CoreV1Api()
 
-namespace = open('/var/run/secrets/kubernetes.io/serviceaccount/namespace').read()
-pod = v1.read_namespaced_pod(os.environ["HOSTNAME"], namespace)
-node = v1.read_node(pod.spec.node_name)
+node_name = os.environ["NODE_NAME"]
+node = v1.read_node(node_name)
 
 context = dict()
 
